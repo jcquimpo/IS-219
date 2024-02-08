fc7067af958fde9a24ef9a9afd28d7d11c7337ac
JanPotat0 <jancquimpo@gmail.com>
2024-02-08 12:09:38 -0500

    homework3 commit

---
+++ homework3/tests/test_calculator.py
@@ -0,0 +1,18 @@
+'''Homework 3 Calculator'''
+from calculator import add, subtract, multiply, divide
+
+def test_addition():
+    '''Test that addition function works '''
+    assert add(2,2) == 4
+
+def test_subtraction():
+    '''Test that subtraction function works '''
+    assert subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that multiply function works '''
+    assert multiply(2,2) == 4
+
+def test_division():
+    '''Test that division function works '''
+    assert divide(2,2) == 1
