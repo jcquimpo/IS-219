891ea6078fc08624f1f0593132a6c430f66f5a73
JanPotat0 <jancquimpo@gmail.com>
2024-02-29 22:13:55 -0500

    added calculator functions

--- calculator_design_patterns_pt2/app/plugins/menu/__init__.py
+++ calculator_design_patterns_pt2/app/plugins/menu/__init__.py
@@ -6,6 +6,9 @@ class MenuCommand(Command):
     def execute(self):
         print(f'List of all commands: ')
         print(f'greet')
-        print(f'goodbye')
         print(f'menu')
         print(f'generate')
+        print(f'add')
+        print(f'subtract')
+        print(f'multiply')
+        print(f'divide')
